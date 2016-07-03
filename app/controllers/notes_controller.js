fb6d22f996047a2d442073fd63c3c61ddb3aa6e6
demiladebamgbose <demiladebamgbose94@gmail.com>
2016-07-03 15:09:49 +0100

    fix indentation

--- app/controllers/notes_controller.js
+++ app/controllers/notes_controller.js
@@ -1,25 +1,25 @@
 var Note = require ('../models/notes_model');
 noteController = {};
 noteController.addNote = function(req, res){
-	Note.findOne({title: req.body.title}, function(err, title){
-		console.log(req.body.title, 'dfdgfghh');
-		if(title){
-			console.log(title, "fvfvfvfv");
-			res.json({
-				success:false,
-				message: 'title taken'
-			});
-		}
-		else{
-			Note.create(req.body, function(err, note){
-				if(err){
-					res.json(err);
-				}
-				console.log("note created");
-				return res.json(note);
-			});
-		}
-	});
+  Note.findOne({title: req.body.title}, function(err, title){
+    console.log(req.body.title, 'dfdgfghh');
+    if(title){
+      console.log(title, "fvfvfvfv");
+      res.json({
+        success:false,
+        message: 'title taken'
+      });
+    }
+    else{
+      Note.create(req.body, function(err, note){
+        if(err){
+          res.json(err);
+        }
+        console.log("note created");
+        return res.json(note);
+      });
+    }
+  });
 };
 
 
